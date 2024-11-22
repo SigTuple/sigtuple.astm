8c7f492e8afedfd7866c2c87ae10ac7bdde66791
maqsudSigtuple <maksud.inamdar@sigtuple.com>
2024-11-22 17:06:19 +0530

    test

--- src/senaite/astm/tests/test_astm_client_protocol.py
+++ src/senaite/astm/tests/test_astm_client_protocol.py
@@ -19,6 +19,16 @@ class ASTMClientProtocolTest(ASTMTestBase):
     async def asyncSetUp(self):
         self.protocol = ASTMClientProtocol()
 
+        self.lines = [
+            b'\x021H|\\^&|||C111^Roche^c111^4.2.2.1730^1^13147|||||host|RSUPL^REAL|P|1|20230727162028\r\x179B\r\n',
+            b'\x022P|1||\r\x174B\r\n',
+            b'\x023O|1||T20-10143GA D07^^2||R||||||N|||||||||||20230727162028|||F\r\x17C0\r\n',
+            b'\x024R|1|^^^550|95.2|U/L||N||F||$SYS$||20230727162028\r\x17A2\r\n',
+            b'\x025C|1|I||I\r\x174F\r\n',
+            b'\x026M|1|RR^BM^c111^1|137|137\\136\\430\\421\\414\\409\\590\\615\\656\\691\\719\\744\\763\\776\\786\\795\\942\\998\\1032\\1055\\1071\\1087\\1106\\1121\\1136\\1154\\1168\\1185\\1199\\1219\\1237\\1252\\1271\\1287|0.005564\r\x17AD\r\n',
+            b'\x027L|1|N\r\x030A\r\n',
+        ]
+
     def get_mock_transport(self, ip="127.0.0.1", port=12345):
         transport = MagicMock()
         transport.get_extra_info = Mock(return_value=(ip, port))
@@ -106,12 +116,8 @@ class ASTMClientProtocolTest(ASTMTestBase):
         # Check that the protocol attribute `last_sent_outbound_message_idx` is -1
         self.assertEqual(self.protocol.last_sent_outbound_message_idx, -1)
 
-        # read instrument file
-        path = self.get_instrument_file_path("yumizen_h500.txt")
-        lines = self.read_file_lines(path)
-
         # initialize outbound communication
-        self.protocol.send_outbound_message(message_to_LIS=lines)
+        self.protocol.send_outbound_message(message_to_LIS=self.lines)
 
         # We expect an ENQ to establish connection
         transport.write.assert_called_with(ENQ)
@@ -128,3 +134,18 @@ class ASTMClientProtocolTest(ASTMTestBase):
         self.assertNotEqual(self.protocol.last_sent_outbound_message_idx, -1)
         self.assertTrue(self.protocol.is_outbound_connection_active)
         self.assertFalse(self.protocol.is_enq_sent_for_outbound_communication)
+
+        idx = 0
+        for line in self.lines:
+            # We expect an ASTM client to send each one by one
+            transport.write.assert_called_with(line)
+
+            # Check the protocol attribute `last_sent_outbound_message_idx`
+            self.assertEqual(self.protocol.last_sent_outbound_message_idx, idx)
+
+            # We expect and ACK as response
+            self.protocol.data_received(ACK)
+
+            idx += 1
+
+        
