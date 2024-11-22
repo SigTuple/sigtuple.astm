915302440751f7eca7f13deb6982d44f8aecc4c7
maqsudSigtuple <maksud.inamdar@sigtuple.com>
2024-11-22 15:08:29 +0530

    astm-server

---
+++ src/senaite/astm/astm_server.py
@@ -0,0 +1,212 @@
+# -*- coding: utf-8 -*-
+
+import argparse
+import asyncio
+import contextlib
+import json
+import logging
+import os
+import sys
+from typing import List
+
+from senaite.astm import codec, lims
+from senaite.astm import logger
+from senaite.astm.astm_pull_consumer_protocol import ASTMPullConsumerProtocol
+
+LOGFILE = "sigtuple-astm-server.log"
+DELAY = 30
+
+async def consume(queue, callback=None):
+    """ASTM Message consumer coroutine function
+    """
+    while True:
+        message = await queue.get()
+        logger.info(f"Message: {message}")
+        if callable(callback):
+            callback(message)
+
+async def send_report_data_to_lis_from_mandara(instances: List[ASTMPullConsumerProtocol]):
+    logger.info("Before send_report_data_to_lis_from_mandara")
+    await asyncio.sleep(30)  # Wait 240 seconds, then stop
+    astm_protocol = instances[0]
+    logger.info("\n\nSimulate sending approved report data to LIS\n\n")
+    with open("src/senaite/astm/tests/data/pentra_xlr.txt", "rb") as file:
+        lines = file.readlines()  # Each line is an element in the list
+        astm_protocol.send_outbound_message(message_to_LIS=lines)
+    logger.info("after opening")
+
+async def send_query_data_to_lis_from_device(instances: List[ASTMPullConsumerProtocol]):
+    logger.info("Before send_query_data_to_lis_from_device")
+    await asyncio.sleep(DELAY)  # Wait DELAY seconds, then stop
+    astm_protocol = instances[0]
+    logger.info("\n\n== Simulate sending query CBC request from device to LIS\n\n")
+    with open("src/senaite/astm/tests/json_data/cellavision_results.json", "r") as file:
+        cellavision_cellalabs_query_json = json.load(file)
+        byte_msgs = codec.iter_encode(cellavision_cellalabs_query_json['data'])
+        astm_protocol.send_outbound_message(message_to_LIS=byte_msgs)
+    logger.info("after opening")
+ 
+
+def main():
+    # Argument parser
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    # Argument groups
+    astm_group = parser.add_argument_group('ASTM SERVER')
+    lims_group = parser.add_argument_group('SIGTUPLE LIMS')
+
+    astm_group.add_argument(
+        '-l',
+        '--listen',
+        type=str,
+        default='0.0.0.0',
+        help='Listen IP address')
+
+    astm_group.add_argument(
+        '-p',
+        '--port',
+        type=str,
+        default='4010',
+        help='Port to connect')
+
+    astm_group.add_argument(
+        '-o',
+        '--output',
+        type=str,
+        help='Output directory to write full messages')
+    
+    astm_group.add_argument(
+        '-j',
+        '--jsonfile',
+        type=argparse.FileType('r'),
+        nargs='+',
+        help='JSON file(s) to send')
+
+    lims_group.add_argument(
+        '-u',
+        '--url',
+        type=str,
+        help='SENAITE URL address including username and password in the '
+             'format: http(s)://<user>:<password>@<senaite_url>')
+
+    lims_group.add_argument(
+        '-c',
+        '--consumer',
+        type=str,
+        default='senaite.core.lis2a.import',
+        help='SENAITE push consumer interface')
+
+    lims_group.add_argument(
+        '-m',
+        '--message-format',
+        type=str,
+        default='json',
+        help='Message format to send to SENAITE. '
+             'Allowed formats: "astm", "lis2a", "json".')
+
+    lims_group.add_argument(
+        '-r',
+        '--retries',
+        type=int,
+        default=3,
+        help='Number of attempts of reconnection when SENAITE '
+             'instance is not reachable. Only has effect when '
+             'argument --url is set')
+
+    lims_group.add_argument(
+        '-d',
+        '--delay',
+        type=int,
+        default=5,
+        help='Time delay in seconds between retries when '
+             'SENAITE instance is not reachable. Only has '
+             'effect when argument --url is set')
+
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        action='store_true',
+        help='Verbose logging')
+
+    parser.add_argument(
+        '--logfile',
+        default=LOGFILE,
+        help='Path to store log files')
+
+    # Parse Arguments
+    args = parser.parse_args()
+
+    if args.logfile:
+        handler = logging.handlers.RotatingFileHandler(
+            args.logfile, maxBytes=5, backupCount=0)
+        # Format each log message like this
+        formatter = logging.Formatter(
+            '%(asctime)s %(levelname)-8s %(message)s')
+        # Attach the formatter to the handler
+        handler.setFormatter(formatter)
+        # Attach the handler to the logger
+        logger.addHandler(handler)
+
+    # Get the current event loop.
+    loop = asyncio.get_event_loop()
+
+    # Set logging
+    # if args.verbose:
+    #     logger.setLevel(logging.DEBUG)
+    # else:
+    #     logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
+    logger.addHandler(logging.StreamHandler())
+
+    # Validate output path
+    output = args.output
+    if output and not os.path.isdir(args.output):
+        logger.error('Output path must be an existing directory')
+        return sys.exit(-1)
+
+    # Validate SIGTUPLE URL
+    url = args.url
+    if url:
+        session = lims.Session(url)
+        logger.info('Checking connection to SIGTUPLE ...')
+        if not session.auth():
+            return sys.exit(-1)
+
+    # Create a ASTM message consumer task to be scheduled concurrently.
+    queue = asyncio.Queue()
+    instances: List[ASTMPullConsumerProtocol] = []
+    
+    loop.create_task(send_query_data_to_lis_from_device(instances=instances))
+
+    # Create a TCP server coroutine listening on port of the host address.
+    # IMPORTANT: We create a new Protocol for every connection!
+    server_coro = loop.create_server(
+        lambda: ASTMPullConsumerProtocol(queue=queue, message_format=args.message_format, instances=instances),
+        host=args.listen, port=args.port)
+
+    # Run until the future (an instance of Future) has completed.
+    server = loop.run_until_complete(server_coro)
+
+    for socket in server.sockets:
+        ip, port = socket.getsockname()
+        logger.info('Starting server on {}:{}'.format(ip, port))
+        logger.info('ASTM server ready to handle connections ...')
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        logger.info('Shutting down server...')
+        all_tasks = asyncio.gather(
+            *asyncio.all_tasks(loop), return_exceptions=True)
+        all_tasks.cancel()
+        with contextlib.suppress(asyncio.CancelledError):
+            loop.run_until_complete(all_tasks)
+        loop.run_until_complete(loop.shutdown_asyncgens())
+    finally:
+        loop.close()
+        logger.info('Server is now down...')
+
+
+if __name__ == '__main__':
+    main()
