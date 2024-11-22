b9d54cf2022ca40f91c929aff5083f04dcdbf943
maqsudSigtuple <maksud.inamdar@sigtuple.com>
2024-11-22 17:50:25 +0530

    cellavision mapper decode

--- src/senaite/astm/tests/test_cellavision_cellalabs.py
+++ src/senaite/astm/tests/test_cellavision_cellalabs.py
@@ -5,6 +5,8 @@ import os
 import re
 from senaite.astm.tests.base import ASTMTestBase
 from senaite.astm import codec, logger
+from senaite.astm.instruments import cellavision_cellalabs
+
 
 class CellaVisionCellaLabsTest(ASTMTestBase):
     """Test Cella Vision Cella Labs
@@ -13,6 +15,9 @@ class CellaVisionCellaLabsTest(ASTMTestBase):
     data: any
 
     async def asyncSetUp(self) -> None:
+
+        self.mapping = cellavision_cellalabs.get_mapping()
+
         pattern = re.compile(r'cellavision_cellalabs.*\.json$')
         for path in self.json_data_files:
             if pattern.search(path):
@@ -30,3 +35,25 @@ class CellaVisionCellaLabsTest(ASTMTestBase):
         self.assertEqual([recs[0]], codec.decode(byte_msgs[0]))
         # self.assertEqual([recs[1]], codec.decode(byte_msgs[1])) TODO: To see verify this test
         self.assertEqual([recs[2]], codec.decode(byte_msgs[2]))
+
+    def test_decode_messages(self):
+        recs = self.data['data']
+        byte_msgs = codec.iter_encode(recs)
+
+        data = {}
+        keys = []
+
+        for line in byte_msgs:
+            records = codec.decode(line)
+
+            self.assertTrue(isinstance(records, list), True)
+            self.assertTrue(len(records) > 0, True)
+
+            record = records[0]
+            rtype = record[0]
+            wrapper = self.mapping[rtype](*record)
+            data[rtype] = wrapper.to_dict()
+            keys.append(rtype)
+
+        for key in keys:
+            self.assertTrue(key in data)
